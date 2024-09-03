b01763c0a51c9946afa5739c326390f9b3482955
Daniel Chong <danielgq.chong@gmail.com>
2024-09-03 20:25:07 +1200

    adding tailwind

--- packages/website/src/components/Folder.tsx
+++ packages/website/src/components/Folder.tsx
@@ -1,10 +1,16 @@
 import { FolderType } from "../types/types";
 
+const folderIndent = "pl-2";
+
 export const Folder = ({ data }: { data: FolderType[] }) => {
-  return data.map((row) => (
-    <>
-      <div key={row.id}>{row.name}</div>
-      {row.children ? <Folder data={row.children} /> : null}
-    </>
-  ));
+  return data.map((row) => {
+    const topLevel = row.children ? folderIndent : "";
+    const folderNameStyle = row.children ? "text-gray-700 pl-2" : "";
+    return (
+      <div key={row.id} className={topLevel}>
+        <div className={folderNameStyle}>{row.name}</div>
+        {row.children ? <Folder data={row.children} /> : null}
+      </div>
+    );
+  });
 };
